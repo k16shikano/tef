65f7c311da16bee996516ea54e234008f2540eb8
keiichirou shikano <k16.shikano@gmail.com>
2009-06-06 12:08:56 +0900

    fix bugs and organise repository

---
+++ parser-utils.scm
@@ -0,0 +1,26 @@
+;;;; utilities for parser
+
+;; [token] -> [token] -> [token] or #f
+(define (match-head ls pattern) 
+  (if (equal? (map (lambda (p l) l) pattern ls) pattern)
+      (drop ls (length pattern))
+      #f))
+
+;; [token] -> [get token] [rest token]
+(define (put-specific-code code finder getter)
+  (lambda (ls)
+    (if (null? ls)
+	'()
+	(if (finder (car ls))
+	    (receive (group unseen)
+		     (getter ls)
+		     (cons `(,code . ,group) unseen))
+	    ls))))
+
+;; are there any values whose first value is not null?
+(define-syntax orvalues
+  (syntax-rules ()
+    ((_) (values '() '()))
+    ((_ v1) v1)
+    ((_ v1 v2 ...) 
+     (if (null? (values-ref v1 0)) (orvalues v2 ...) v1))))
